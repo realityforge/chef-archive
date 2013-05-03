e60623bd91922c2b5e601b17c7d42f5259be0f7c
Peter Donald <peter@realityforge.org>
2013-05-03 15:50:39 +1000

    Add some basic doco for the LWRP

--- resources/default.rb
+++ resources/default.rb
@@ -14,24 +14,77 @@
 # limitations under the License.
 #
 
+=begin
+#<
+The LWRP downloads an archive and places in a local versioned directory and
+symlinks the current version.
+
+The LWRP first creates a container directory based on the name and the prefix. Under the
+container directory, there is a directory in which all the versions of the artifact are
+stored. The LWRP will download the artifact and place it in this directory and then symlink
+the "current" directory to the downloaded artifact.
+
+By default the LWRP will retain the directory for the last artifact downloaded.
+
+@action add Download and extract archive.
+
+@section Examples
+
+    # Download the myapp.zip archive, extract, strip the
+    # top level dir and place results into /usr/loca/myapp/pkg/1.0
+    # and symlink /usr/loca/myapp/pkg/current to /usr/loca/myapp/pkg/1.0
+    archive 'myapp' do
+      url "http://example.com/myapp.zip"
+      version '1.0'
+      owner 'myapp'
+      group 'myapp'
+      extract_action 'unzip_and_strip_dir'
+    end
+
+    # Download the myapp.jar and place set the attribute
+    # myapp.home_dir to the container dir (i.e. /usr/local/myapp) and
+    # myapp.jar_location to the downloaded jar. (i.e. /usr/loca/myapp/pkg/current/myapp-1.0.jar)
+    archive 'myapp' do
+      url "http://example.com/myapp.jar"
+      version '1.0'
+      owner 'myapp'
+      group 'myapp'
+      publish_container_dir_to 'myapp.home_dir'
+      publish_artifact_location_to 'myapp.jar_location'
+    end
+
+#>
+=end
+
 actions :add
 
+#<> @attribute name The logical name of the artifact. Used when creating the container directory.
 attribute :name, :kind_of => String, :name_attribute => true
+#<> @attribute url The url from which to download the resource.
 attribute :url, :kind_of => String, :required => true
+#<> @attribute version The version of the archive. Should be set, otherwise will be derived as a hash of the url parameter.
 attribute :version, :kind_of => String, :default => nil
+#<> @attribute owner The owner of the container directory and created artifacts.
 attribute :owner, :kind_of => String, :default => 'root'
+#<> @attribute group The group of the container directory and created artifacts.
 attribute :group, :kind_of => [String, Fixnum], :default => 0
+#<> @attribute umask The umask used when setting up the archive.
 attribute :umask, :kind_of => String, :default => nil
 
-attribute :prefix, :kind_of => String, :default => nil
+#<> @attribute prefix The directory in which the archive is unpacked.
+attribute :prefix, :kind_of => String, :default => '/usr/local'
+#<> @attribute extract_action The action to take with the downloaded archive. Defaults to leaving the archive un-extracted but can also unzip or unzip and stript the first directory.
 attribute :extract_action, :equal_to => ['unzip', 'unzip_and_strip_dir', nil], :default => nil
+
+#<> @attribute publish_container_dir_to The dot separated node attribute to set to the container directory. This occurs at resource definition time.
 attribute :publish_container_dir_to, :kind_of => String, :default => nil
+#<> @attribute publish_artifact_location_to The dot separated node attribute to set to the artifact location. This occurs at resource definition time.
 attribute :publish_artifact_location_to, :kind_of => String, :default => nil
 
 default_action :add
 
 def base_directory
-  "#{(prefix || '/usr/local')}/#{name}"
+  "#{prefix}/#{name}"
 end
 
 def derived_version
