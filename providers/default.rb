665c3497b3a13c9456fa14f47075822db51cc160
Peter Donald <peter@realityforge.org>
2014-10-03 12:57:22 +1000

    Rework the archive process to avoid the creation of a .version file and to never download artifacts if will not be used

--- providers/default.rb
+++ providers/default.rb
@@ -18,52 +18,57 @@ use_inline_resources
 
 action :add do
   current_directory = "#{new_resource.package_directory}/current"
-  version_file = "#{new_resource.target_directory}/.version"
-  check_proc = Proc.new { ::File.exists?(version_file) }
+  archive_exists = ::File.exists?(new_resource.target_artifact)
 
-  cached_package_filename = nil
-  delete_cached_package = true
-  if new_resource.url =~ /^file\:\/\//
-    cached_package_filename = new_resource.url[7, new_resource.url.length]
-    delete_cached_package = false
-  else
-    cached_package_filename = "#{Chef::Config[:file_cache_path]}/#{new_resource.local_filename}"
+  Chef::Log.info "Archive #{new_resource.name} => #{new_resource.target_artifact} Exists? #{archive_exists}"
 
-    remote_file cached_package_filename do
-      not_if { check_proc.call }
-      source new_resource.url
-      unless node['platform'] == 'windows'
-        owner new_resource.owner
-        group new_resource.group
-        mode '0600'
+  unless archive_exists
+
+    cached_package_filename = nil
+    delete_cached_package = true
+    if new_resource.url =~ /^file\:\/\//
+      cached_package_filename = new_resource.url[7, new_resource.url.length]
+      delete_cached_package = false
+    else
+      cached_package_filename = "#{Chef::Config[:file_cache_path]}/#{new_resource.local_filename}"
+
+      remote_file cached_package_filename do
+        not_if { archive_exists }
+        source new_resource.url
+        unless node['platform'] == 'windows'
+          owner new_resource.owner
+          group new_resource.group
+          mode '0600'
+        end
+        action :create_if_missing
       end
-      action :create_if_missing
     end
-  end
 
-  [new_resource.base_directory, new_resource.package_directory, new_resource.target_directory].each do |dir|
-    directory dir do
-      unless node['platform'] == 'windows'
-        owner new_resource.owner
-        group new_resource.group
-        mode '0700'
+    [new_resource.base_directory, new_resource.package_directory, new_resource.target_directory].each do |dir|
+      directory dir do
+        unless node['platform'] == 'windows'
+          owner new_resource.owner
+          group new_resource.group
+          mode '0700'
+        end
+        recursive (new_resource.base_directory == dir)
+        action :create
+        not_if { ::File.exists?(new_resource.base_directory) && dir == new_resource.base_directory }
       end
-      recursive (new_resource.base_directory == dir)
-      action :create
     end
-  end
 
-  if 'unzip_and_strip_dir' == new_resource.extract_action
-    temp_dir = "/tmp/install-#{new_resource.name}-#{new_resource.derived_version}"
+    if 'unzip_and_strip_dir' == new_resource.extract_action
+      temp_dir = "/tmp/install-#{new_resource.name}-#{new_resource.derived_version}"
 
-    package 'unzip'
+      package 'unzip'
 
-    bash 'unzip_package' do
-      not_if { check_proc.call }
-      user new_resource.owner
-      group new_resource.group
-      umask new_resource.umask if new_resource.umask
-      code <<-CMD
+      bash 'unzip_package' do
+        not_if { archive_exists }
+        user new_resource.owner
+        group new_resource.group
+        umask new_resource.umask if new_resource.umask
+        code <<-CMD
+          set -e
           mkdir #{temp_dir}
           unzip -q -u -o #{cached_package_filename} -d #{temp_dir}
           if [ `ls -1 #{temp_dir} |wc -l` -gt 1 ] ; then
@@ -71,52 +76,43 @@ action :add do
             exit 37
           fi
           mv #{temp_dir}/*/* #{new_resource.target_artifact} && rm -rf #{temp_dir} && test -d #{new_resource.target_artifact}
-      CMD
-    end
-  elsif 'unzip' == new_resource.extract_action
-    package 'unzip'
-
-    bash 'unzip_package' do
-      not_if { check_proc.call }
-      user new_resource.owner
-      group new_resource.group
-      umask new_resource.umask if new_resource.umask
-      code "unzip -q -u -o #{cached_package_filename} -d #{new_resource.target_artifact}"
-    end
-  elsif new_resource.extract_action.nil?
-    if node['platform'] == 'windows'
-      windows_batch 'move_package' do
-        not_if { check_proc.call }
-        code "cp #{cached_package_filename} #{new_resource.target_artifact}"
+        CMD
       end
-    else
-      bash 'move_package' do
-        not_if { check_proc.call }
+    elsif 'unzip' == new_resource.extract_action
+      package 'unzip'
+
+      bash 'unzip_package' do
+        not_if { archive_exists }
         user new_resource.owner
         group new_resource.group
         umask new_resource.umask if new_resource.umask
-        code "cp #{cached_package_filename} #{new_resource.target_artifact}"
+        code "unzip -q -u -o #{cached_package_filename} -d #{new_resource.target_artifact}"
+      end
+    elsif new_resource.extract_action.nil?
+      if node['platform'] == 'windows'
+        windows_batch 'move_package' do
+          not_if { archive_exists }
+          code "cp #{cached_package_filename} #{new_resource.target_artifact}"
+        end
+      else
+        bash 'move_package' do
+          not_if { archive_exists }
+          user new_resource.owner
+          group new_resource.group
+          umask new_resource.umask if new_resource.umask
+          code "cp #{cached_package_filename} #{new_resource.target_artifact}"
+        end
+      end
+    else
+      raise "Unsupported extract_action #{new_resource.extract_action}"
+    end
+
+    if delete_cached_package
+      file cached_package_filename do
+        backup false
+        action :delete
       end
     end
-  else
-    raise "Unsupported extract_action #{new_resource.extract_action}"
-  end
-
-  if delete_cached_package
-    file cached_package_filename do
-      backup false
-      action :delete
-    end
-  end
-
-  file version_file do
-    unless node['platform'] == 'windows'
-      owner new_resource.owner
-      group new_resource.group
-      mode '0600'
-    end
-    content new_resource.derived_version
-    action :create
   end
 
   last_version = nil
